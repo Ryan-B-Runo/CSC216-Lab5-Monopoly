3b0a76d1f5a9b68f9f9e2dd3ee5b11ed54d9dd1d
Ryan-B-Runo <43120875+Ryan-B-Runo@users.noreply.github.com>
2025-06-22 23:52:46 -0500

    Add the testing code

---
+++ Driver.java
@@ -0,0 +1,32 @@
+public class Driver {
+
+    public static void main(String[] args) {
+//        CircularLinkedList monopolyBoard = new CircularLinkedList<string>();
+//
+//        monopolyBoard.append("Go");
+//        monopolyBoard.append("Mediteranean Avenue")
+//        monopolyBoard.append("Community Chest")
+//        monopolyBoard.append("Baltic Avenue")
+//        monopolyBoard.append("Income Tax")
+//// And so on for all of the spaces on the board...
+//
+//        System.out.println(monopolyBoard.currentNode); // This should print "Go" to the console
+//
+//        monopolyBoard.step();
+//
+//        System.out.println(monopolyBoard.currentNode); // This should print "Mediteranean Avenue" to the console
+//
+//        monopolyBoard.step();
+//        monopolyBoard.step();
+//        monopolyBoard.step();
+//
+//        System.out.println(monopolyBoard.currentNode); // This should print "Income Tax" to the console
+//
+//        for(int i = 0; i < 37 i++) {
+//            monopolyBoard.step();
+//        }
+//
+//        System.out.println(monopolyBoard.currentNode); // This should print "Mediteranean Avenue" to the console since we have looped back around
+    }
+
+}
